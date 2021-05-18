ad294557d346ca4937a7fd30f63bdc81e120e723
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-05-18 14:43:40 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from "helmet";
-
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: "http://localhost:8080", credentials: true });
   app.use(helmet());
   
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
